db67e810d8ed0b52a2dad707d8aeb70168fd1f21
Jakob Leczinsky <jakob.leczinsky@gmail.com>
2023-10-24 21:06:02 +0200

    automatically start Sway at login

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -1,14 +1,18 @@
 if status is-interactive
     # Commands to run in interactive sessions can go here
-  fzf_key_bindings
+    fzf_key_bindings
 
-  set -gx FZF_DEFAULT_COMMAND 'rg --files'
-  set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
-  set -gx FZF_ALT_C_COMMAND "fd --type directory . $HOME"
-  set -gx FZF_ALT_C_OPTS "--preview 'tree -C {} | head -200'"
-  set -gx FZF_CTRL_T_OPTS "--preview 'highlight -O ansi -l {} 2> /dev/null'"
+    set -gx FZF_DEFAULT_COMMAND 'rg --files'
+    set -gx FZF_CTRL_T_COMMAND "$FZF_DEFAULT_COMMAND"
+    set -gx FZF_ALT_C_COMMAND "fd --type directory . $HOME"
+    set -gx FZF_ALT_C_OPTS "--preview 'tree -C {} | head -200'"
+    set -gx FZF_CTRL_T_OPTS "--preview 'highlight -O ansi -l {} 2> /dev/null'"
 
-  source ~/.aliases
+    source ~/.aliases
 
-  #fnm env | source
+    #fnm env | source
+
+    if test (tty) = /dev/tty1
+        sway
+    end
 end
